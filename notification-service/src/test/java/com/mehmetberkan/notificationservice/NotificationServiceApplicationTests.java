88ec5d4cd2a712dd366b2c47ef6c98339b933fe6
Mehmet Berkan <mehmetberkangorel@gmail.com>
2025-03-16 00:20:56 +0300

    Initial commit

---
+++ notification-service/src/test/java/com/mehmetberkan/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetberkan.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
